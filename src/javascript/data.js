aa8f5102f654ffb01aa5c291b487d2235f0799a6
Adam Pash <adam.pash@gmail.com>
2015-02-27 14:15:37 -0500

    Initial commit

---
+++ src/javascript/data.js
@@ -0,0 +1,142 @@
+DATA = [
+  {
+    "Train Line":"A",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"30.40%",
+    "How Often It Was Late In 2013 By Wait Assessment":"26.30%",
+    "How Often It Was Late In 2014 By Terminal Delay":"27.50%",
+    "How Often It Was Late In 2013 By Terminal Delay":"16.80%"
+  },
+  {
+    "Train Line":"B",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"20.10%",
+    "How Often It Was Late In 2013 By Wait Assessment":"20.90%",
+    "How Often It Was Late In 2014 By Terminal Delay":"25.40%",
+    "How Often It Was Late In 2013 By Terminal Delay":"16.80%"
+  },
+  {
+    "Train Line":"C",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"19.00%",
+    "How Often It Was Late In 2013 By Wait Assessment":"17%",
+    "How Often It Was Late In 2014 By Terminal Delay":"16.30%",
+    "How Often It Was Late In 2013 By Terminal Delay":"8.10%"
+  },
+  {
+    "Train Line":"D",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"17.70%",
+    "How Often It Was Late In 2013 By Wait Assessment":"20.60%",
+    "How Often It Was Late In 2014 By Terminal Delay":"24.90%",
+    "How Often It Was Late In 2013 By Terminal Delay":"16.40%"
+  },
+  {
+    "Train Line":"E",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"25.50%",
+    "How Often It Was Late In 2013 By Wait Assessment":"25.00%",
+    "How Often It Was Late In 2014 By Terminal Delay":"28.10%",
+    "How Often It Was Late In 2013 By Terminal Delay":"18.60%"
+  },
+  {
+    "Train Line":"F",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"29.00%",
+    "How Often It Was Late In 2013 By Wait Assessment":"24.80%",
+    "How Often It Was Late In 2014 By Terminal Delay":"45.10%",
+    "How Often It Was Late In 2013 By Terminal Delay":"27.10%"
+  },
+  {
+    "Train Line":"G",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"19.10%",
+    "How Often It Was Late In 2013 By Wait Assessment":"17.80%",
+    "How Often It Was Late In 2014 By Terminal Delay":"21.90%",
+    "How Often It Was Late In 2013 By Terminal Delay":"13.90%"
+  },
+  {
+    "Train Line":"J/Z",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"18.30%",
+    "How Often It Was Late In 2013 By Wait Assessment":"19.30%",
+    "How Often It Was Late In 2014 By Terminal Delay":"13.60%",
+    "How Often It Was Late In 2013 By Terminal Delay":"5.70%"
+  },
+  {
+    "Train Line":"L",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"18%",
+    "How Often It Was Late In 2013 By Wait Assessment":"18.30%",
+    "How Often It Was Late In 2014 By Terminal Delay":"6.10%",
+    "How Often It Was Late In 2013 By Terminal Delay":"5.60%"
+  },
+  {
+    "Train Line":"M",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"21.40%",
+    "How Often It Was Late In 2013 By Wait Assessment":"21.10%",
+    "How Often It Was Late In 2014 By Terminal Delay":"25.10%",
+    "How Often It Was Late In 2013 By Terminal Delay":"14.60%"
+  },
+  {
+    "Train Line":"N",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"19.10%",
+    "How Often It Was Late In 2013 By Wait Assessment":"19.70%",
+    "How Often It Was Late In 2014 By Terminal Delay":"28.80%",
+    "How Often It Was Late In 2013 By Terminal Delay":"17.80%"
+  },
+  {
+    "Train Line":"Q",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"20.00%",
+    "How Often It Was Late In 2013 By Wait Assessment":"21.10%",
+    "How Often It Was Late In 2014 By Terminal Delay":"24.30%",
+    "How Often It Was Late In 2013 By Terminal Delay":"13.50%"
+  },
+  {
+    "Train Line":"R",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"18.70%",
+    "How Often It Was Late In 2013 By Wait Assessment":"19.20%",
+    "How Often It Was Late In 2014 By Terminal Delay":"17.20%",
+    "How Often It Was Late In 2013 By Terminal Delay":"11.70%"
+  },
+  {
+    "Train Line":"1",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"20.20%",
+    "How Often It Was Late In 2013 By Wait Assessment":"17.90%",
+    "How Often It Was Late In 2014 By Terminal Delay":"21.30%",
+    "How Often It Was Late In 2013 By Terminal Delay":"15.60%"
+  },
+  {
+    "Train Line":"2",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"28.30%",
+    "How Often It Was Late In 2013 By Wait Assessment":"26.80%",
+    "How Often It Was Late In 2014 By Terminal Delay":"53.40%",
+    "How Often It Was Late In 2013 By Terminal Delay":"44.00%"
+  },
+  {
+    "Train Line":"3",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"22.60%",
+    "How Often It Was Late In 2013 By Wait Assessment":"22.20%",
+    "How Often It Was Late In 2014 By Terminal Delay":"33.80%",
+    "How Often It Was Late In 2013 By Terminal Delay":"28.50%"
+  },
+  {
+    "Train Line":"4",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"28.60%",
+    "How Often It Was Late In 2013 By Wait Assessment":"26.30%",
+    "How Often It Was Late In 2014 By Terminal Delay":"54.20%",
+    "How Often It Was Late In 2013 By Terminal Delay":"45.30%"
+  },
+  {
+    "Train Line":"5",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"32.60%",
+    "How Often It Was Late In 2013 By Wait Assessment":"29.30%",
+    "How Often It Was Late In 2014 By Terminal Delay":"57.00%",
+    "How Often It Was Late In 2013 By Terminal Delay":"43.60%"
+  },
+  {
+    "Train Line":"6",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"31.40%",
+    "How Often It Was Late In 2013 By Wait Assessment":"23.40%",
+    "How Often It Was Late In 2014 By Terminal Delay":"46.20%",
+    "How Often It Was Late In 2013 By Terminal Delay":"27.60%"
+  },
+  {
+    "Train Line":"7",
+    "How Often It Was Late In 2014 By Wait Assessment (%)":"24.30%",
+    "How Often It Was Late In 2013 By Wait Assessment":"21.40%",
+    "How Often It Was Late In 2014 By Terminal Delay":"12.00%",
+    "How Often It Was Late In 2013 By Terminal Delay":"11.40%"
+  }
+]
